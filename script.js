cfb0c8eb5f7fffe69e303279811cc2d81dd29b4a
bergario <bemby.ber@gmail.com>
2020-09-29 16:10:43 +0700

    Fucntion Declaration

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // 1. Object Literal
 
-let Person = {
-    name: "Bergario",
-    energy: 10,
-    booster: function (value = 0) {
-        this.energy = this.energy + value;
-        console.log(`${this.name} have energy: ${this.energy}`);
-    },
-    attack: function (hit) {
-        this.energy = this.energy - hit;
-        console.log(`${this.name} have been attact: ${hit} -> energy:${this.energy}`)
+// let Person = {
+//     name: "Bergario",
+//     energy: 10,
+//     booster: function (health = 0) {
+//         this.energy = this.energy + health;
+//         console.log(`${this.name} have energy: ${this.energy}`);
+//     },
+//     attack: function (hit) {
+//         this.energy = this.energy - hit;
+//         console.log(`${this.name} have been attack: ${hit} -> energy:${this.energy}`)
+//     }
+// }
+
+
+
+
+// 2. Function Declaration
+function Person(name, energy) {
+    let person = {};
+    person.name = name;
+    person.energy = energy;
+
+    person.booster = function (health) {
+
+        this.energy += health;
+        console.log(`${this.name} add energy:${health} -> ${this.energy}`);
+
     }
+
+    person.attack = function (hit) {
+        this.energy -= hit;
+        console.log(`${this.name} have been attack: ${hit} -> energy:${this.energy}`);
+    }
+    return person;
 }
+
+let bergario = Person('bergario', 50);
+let batman = Person('batman', 30);
