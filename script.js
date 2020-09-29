728ae9b74617ebabf30bc87123bddce98ab57e78
bergario <bemby.ber@gmail.com>
2020-09-29 23:06:19 +0700

    Prototype & Class version

--- script.js
+++ script.js
@@ -62,32 +62,88 @@
 // let bergario = new Person('bergario', 50);
 // let superman = new Person('superman', 100);
 
+
+
 // 4. Object Create
 
-const methodPerson = {
+// const methodPerson = {
 
-    booster: function (health) {
+//     booster: function (health) {
 
-        this.energy += health;
-        console.log(`${this.name} add energy:${health} -> ${this.energy}`);
+//         this.energy += health;
+//         console.log(`${this.name} add energy:${health} -> ${this.energy}`);
 
-    },
+//     },
 
-    attack: function (hit) {
-        this.energy -= hit;
-        console.log(`${this.name} have been attack: ${hit} -> energy:${this.energy}`);
-    }
+//     attack: function (hit) {
+//         this.energy -= hit;
+//         console.log(`${this.name} have been attack: ${hit} -> energy:${this.energy}`);
+//     }
 
-}
+// }
+
+// function Person(name, energy) {
+//     let person = Object.create(methodPerson);
+//     person.name = name;
+//     person.energy = energy;
+
+
+//     return person;
+// }
+
+// let bergario = Person('bergario', 50);
+// let batman = Person('batman', 30);
+
+
+
+// 5. Protoype
 
 function Person(name, energy) {
-    let person = Object.create(methodPerson);
-    person.name = name;
-    person.energy = energy;
 
+    // dibalik layar dibuat deafault --> let this = Object.create(Person.prototype)
 
-    return person;
+    this.name = name;
+    this.energy = energy;
 }
 
-let bergario = Person('bergario', 50);
-let batman = Person('batman', 30);
+Person.prototype.booster = function (health) {
+
+    this.energy += health;
+    console.log(`${this.name} add energy:${health} -> ${this.energy}`);
+
+}
+
+Person.prototype.attack = function (hit) {
+
+    this.energy -= hit;
+    console.log(`${this.name} have been attack: ${hit} -> energy:${this.energy}`);
+}
+
+let bergario = new Person("bergario", 90);
+
+
+
+// 6. Class Version
+
+class Person {
+
+    constructor(name, energy) {
+        this.name = name;
+        this.energy = energy;
+    }
+
+    booster(health) {
+
+        this.energy += health;
+        return (`${this.name} add energy: ${health} -> ${this.energy}`);
+
+    }
+
+    attack(hit) {
+
+        this.energy -= hit;
+        return (`${this.name} have been atttack: ${hit} -> energy: ${this.energy}`);
+    }
+}
+
+let bergario = new Person('bergario', 90);
